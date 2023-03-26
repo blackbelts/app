b4f4b0e2657d2d2462e85b326718765992f39102
Nazeeh Salah <nazeehsalah28@gmail.com>
2023-03-26 15:34:44 +0200

    first commit

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,6 @@
+importScripts('https://www.gstatic.com/firebasejs/5.4.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.4.1/firebase-messaging.js');
+firebase.initializeApp({
+  'messagingSenderId': "1006913264699"
+});
+const messaging = firebase.messaging();
